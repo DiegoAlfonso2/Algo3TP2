a27564e84bddb23712ccb0ac5285bd603abcc3c8
Agustin Cabrera <agustin.cabrera@hotmail.com>
2016-06-09 17:45:12 -0300

    Informe para la segunda con el diagrama de clases

--- src/fiuba/algo3/modelo/superficies/Superficie.java
+++
@@ -1,9 +0,0 @@
-package fiuba.algo3.modelo.superficies;
-
-import fiuba.algo3.modelo.transformers.AlgoFormer;
-
-public abstract class Superficie {
-	
-	public abstract void actuarSobreAlgoformer(AlgoFormer personaje);
-
-}
