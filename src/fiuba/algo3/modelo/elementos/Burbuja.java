2a4e08b5249f61f0021e1ffbe0f0d018fdaac753
Agustin Cabrera <agustin.cabrera@hotmail.com>
2016-06-20 23:24:20 -0300

    Interfaz grafica y una carpeta con todas las imagenes

--- src/fiuba/algo3/modelo/elementos/Burbuja.java
+++
@@ -1,5 +0,0 @@
-package fiuba.algo3.modelo.elementos;
-
-public class Burbuja extends Bonus {
-
-}
--- src/fiuba/algo3/modelo/superficies/Aire.java
+++
@@ -1,5 +0,0 @@
-package fiuba.algo3.modelo.superficies;
-
-public abstract class Aire extends Superficie {
-	
-}
