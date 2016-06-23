1d8b8f07709073df9adcff751e6b709da20b43a8
Agustin Cabrera <agustin.cabrera@hotmail.com>
2016-06-23 10:19:13 -0300

    Interfaz

--- src/fiuba/algo3/modelo/elementos/Flash.java
+++
@@ -1,5 +0,0 @@
-package fiuba.algo3.modelo.elementos;
-
-public class Flash extends Bonus {
-
-}
--- src/fiuba/algo3/modelo/excepciones/PosicionInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package fiuba.algo3.modelo.excepciones;
-
-public class PosicionInvalidaException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
